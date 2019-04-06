648ee4cfb891522fbc5e9ed0228cac81338895df
callumfairbairn <callum.fairbairn@thoughtworks.com>
2019-04-06 21:48:03 +0100

    Made snake1 move

--- src/Game.js
+++ src/Game.js
@@ -10,16 +10,37 @@ export class Game extends Component {
         let sizeX = 20, sizeY = 20;
         let size = new Coord(sizeX, sizeY);
 
-        let startingPosition1 = new Coord(size.x-5, size.y/2);
-        let startingPosition2 = new Coord(5, size.y/2);
+        let startingPosition1 = new Coord(4, size.y/2);
+        let startingPosition2 = new Coord(size.x-5, size.y/2);
 
         let snake1 = new Snake(size, startingPosition1);
         let snake2 = new Snake(size, startingPosition2);
+        let snakes = {snake1, snake2};
 
-        this.snakes = {snake1, snake2};
+        this.player1KeyPressArray = [];
+        this.player2KeyPressArray = [];
+
         this.state = {
             size: size,
+            snakes: snakes,
         };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            150
+        );
+    }
+
+    snakeLocationCalculation(snake) {
+        let newSnake = snake;
+        // the -1 here could cause problems later on
+        for (let i = snake.location.length-1; i > 0; i--) {
+            newSnake.location[i] = [newSnake.location[i-1].x, newSnake.location[i-1].y ]
+        }
+        newSnake.updateHeadLocation();
+        return newSnake;
     }
 
     makeRows() {
@@ -27,18 +48,85 @@ export class Game extends Component {
         for (let y = 0; y < this.state.size.y; y++) {
             rows.push(<Row
                 size={this.state.size}
-                snakes={this.snakes}
+                snakes={this.state.snakes}
                 row={y}
             />);
         }
         return rows;
     }
 
+    handleKeyPress(event) {
+        let eventCode = event.charCode || event.keyCode;
+
+        let playerMap = {
+            37: 'player1',
+            39: 'player1',
+            38: 'player1',
+            40: 'player1',
+            65: 'player2',
+            68: 'player2',
+            83: 'player2',
+            87: 'player2',
+        };
+
+        if (playerMap[eventCode] === 'player1') {
+            this.player1KeyPressArray.push(eventCode);
+        } else {
+            this.player2KeyPressArray.push(eventCode);
+        }
+    }
+
+    handleKeyPressArray(player) {
+        if (player === 1) {
+            if (!this.player1KeyPressArray[0]) {
+                return
+            }
+
+            let eventCode = this.player1KeyPressArray[0];
+
+            let directionMap = {
+                37:(this.state.snakes.snake1.direction !== 'right') ? 'left' : 'right',
+                39:(this.state.snakes.snake1.direction !== 'left') ? 'right' : 'left',
+                38:(this.state.snakes.snake1.direction !== 'down') ? 'up' : 'down',
+                40:(this.state.snakes.snake1.direction !== 'up') ? 'down': 'up',
+            };
+
+            let newDirection = directionMap[eventCode];
+
+            if (newDirection) {
+                let snakes = this.state.snakes;
+                let snake1 = snakes.snake1;
+                snake1.direction = newDirection;
+                snakes.snake1 = snake1;
+
+                this.setState({
+                    snakes: snakes,
+                });
+            }
+
+            this.player1KeyPressArray = this.player1KeyPressArray.slice(1);
+        }
+    }
+
+    tick() {
+        this.handleKeyPressArray(1);
+        this.handleKeyPressArray(2);
+
+        let snakes = this.state.snakes;
+        // NEED TO GIVE THIS DIRECTION, OR USE SNAKE'S OWN DIRECTION
+        snakes.snake1 = this.snakeLocationCalculation(this.state.snakes.snake1);
+
+        this.setState({
+            snakes: snakes,
+        })
+    }
+
     render() {
         let rows = this.makeRows();
+
         return (
             <div id='gameContainer'>
-                <div>
+                <div onKeyDown={(event) => this.handleKeyPress(event)}>
                     {rows}
                 </div>
             </div>
--- src/Snake.js
+++ src/Snake.js
@@ -1,12 +1,43 @@
 export class Snake {
     constructor(size, startingPosition) {
         this.length = 1;
-        this.speed = 0;
+        this.speed = 1;
         this.direction = 'none';
         this.location = [startingPosition];
+
+        // this.locationMap = {
+        //     'left': this.moveLeft(),
+        //     'right': this.moveRight(),
+        //     'up': this.moveUp(),
+        //     'down': this.moveDown(),
+        // };
     }
 
-    getHeadLocation() {
-        return this.location[0];
+    updateHeadLocation() {
+        if (this.direction === 'left') {
+            this.moveLeft();
+        } else if (this.direction === 'right') {
+            this.moveRight();
+        } else if (this.direction === 'up') {
+            this.moveUp();
+        } else if (this.direction === 'down') {
+            this.moveDown();
+        }
+    }
+
+    moveUp() {
+        this.location[0].y--;
+    }
+
+    moveDown() {
+        this.location[0].y++;
+    }
+
+    moveLeft() {
+        this.location[0].x--;
+    }
+
+    moveRight() {
+        this.location[0].x++;
     }
 }
